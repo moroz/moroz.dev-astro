eb1619f08ec5c2f0d602063c38bd9ea82c0bafac
Karol Moroz <karol@moroz.dev>
2023-02-25 22:39:17 +0100

    Implement hero section

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import path from "path";
 
 // https://astro.build/config
 import svelte from "@astrojs/svelte";
@@ -7,8 +8,16 @@ import svelte from "@astrojs/svelte";
 export default defineConfig({
   markdown: {
     shikiConfig: {
-      theme: "dracula"
-    }
+      theme: "dracula",
+    },
   },
-  integrations: [svelte()]
+  integrations: [svelte()],
+  vite: {
+    resolve: {
+      alias: {
+        "@styles": path.resolve(process.cwd(), "src/styles"),
+      },
+    },
+  },
 });
+
