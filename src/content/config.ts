f2e14eb7a2a965e4c70d8b753400faadfdf43271
Karol Moroz <karol@moroz.dev>
2023-03-04 15:11:58 +0100

    Add video content

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,8 +9,18 @@ const blogCollection = defineCollection({
   }),
 });
 
+const videoCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    youtube: z.string(),
+    date: z.date(),
+    description: z.string().optional(),
+  }),
+});
+
 export const collections = {
   blog: blogCollection,
+  video: videoCollection,
 };
 
 export const POSTS_PER_PAGE = 20;
--- src/lib/blog.ts
+++ src/lib/blog.ts
@@ -1,14 +1,21 @@
-import { getCollection } from "astro:content";
+import { getCollection, CollectionEntry } from "astro:content";
 
 const isDev = process.env.NODE_ENV !== "production";
 
+const sorter = (a: CollectionEntry<any>, b: CollectionEntry<any>) => {
+  if (a.data.date > b.data.date) return -1;
+  if (a.data.date < b.data.date) return 1;
+  if (a.slug > b.slug) return -1;
+  if (a.slug < b.slug) return 1;
+  return 0;
+};
+
 export const getSortedPosts = async () => {
   const posts = await getCollection("blog", ({ data }) => isDev || !data.draft);
-  return posts.sort((a, b) => {
-    if (a.data.date > b.data.date) return -1;
-    if (a.data.date < b.data.date) return 1;
-    if (a.slug > b.slug) return -1;
-    if (a.slug < b.slug) return 1;
-    return 0;
-  });
+  return posts.sort(sorter);
+};
+
+export const getSortedVideos = async () => {
+  const videos = await getCollection("videos");
+  return videos.sort(sorter);
 };
